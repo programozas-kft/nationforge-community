7d941ff413cf1f0b89826c4fadafbe0f0aa92850
programozas <programozas.kft@gmail.com>
2026-05-08 22:37:15 +0200

    fix: set from address in CampaignMail from campaign record

--- app/Mail/CampaignMail.php
+++ app/Mail/CampaignMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use App\Models\EmailCampaign;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -17,7 +18,11 @@ class CampaignMail extends Mailable
 
     public function envelope(): Envelope
     {
+        $fromEmail = $this->campaign->from_email ?: config('mail.from.address');
+        $fromName  = $this->campaign->from_name  ?: config('mail.from.name');
+
         return new Envelope(
+            from: new Address($fromEmail, $fromName),
             subject: $this->campaign->subject,
         );
     }
